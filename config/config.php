964136b77df2e3be672f925273ff5fc8f8e6803c
unknown <farith2699@gmail.com>
2023-11-23 15:12:53 -0500

    cambio3

--- config/config.php
+++ config/config.php
@@ -1,4 +1,16 @@
 <?php
-define('CLIENT_ID', '');
-define('LOCALE', 'es_ES');
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "COP";
+const CLIENT_ID = "AfNmcQZ_GOSJdtBQupvGAgzUEb6jmzIOF_X0gmaKwyVWviZ9iiE4Eo-Cx_HK_mS85E2vC3OiuQWyXR89";
+
+const USER_SMTP = "farith2699@gmail.com";
+const PASS_SMTP = "ezwkydwcgcrhcfsw";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
 ?>
